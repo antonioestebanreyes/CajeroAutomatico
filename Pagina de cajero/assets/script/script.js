8498d4de6efb4179732b312b950e3457198f96ca
Antonio <antonioestebanreyes8@gmail.com>
2022-09-23 00:22:07 -0500

    fix:Mejora al cajero automatico

--- Pagina de cajero/assets/script/script.js
+++ Pagina de cajero/assets/script/script.js
@@ -1,26 +1,24 @@
-var retiro = document.getElementById("Retiro")
-var boton = document.getElementById("botton")
+var retiro = document.getElementById("Retiro");
+var boton = document.getElementById("botton");
 var NombreDeUsuario = document.getElementById("Nombre");
 var EstoDecuentActual = document.getElementById("EstodoDeCuenta");
 var MontoRetirado = document.getElementById("MontoRetirado");
 var SaldoActualizado = document.getElementById("SaldoActualizado");
-var aler = document.getElementById("aler")
-var cera = document.getElementById("cerrar")
-var boton2 = document.getElementById("botto")
-var menu = document.getElementById("menu")
+var aler = document.getElementById("aler");
+var cera = document.getElementById("cerrar");
+var boton2 = document.getElementById("botto");
+var menu = document.getElementById("menu");
 var usuario = document.getElementById("usuario");
-var m_nombre_de_usurio=document.getElementById("m_nombre_de_usurio")
-var m_estado_de_cuenta=document.getElementById("m_estado_de_cuenta")
-var volver=document.getElementById("volver")
-boton.addEventListener("click", evaluar)
-cera.addEventListener("click", cerar)
-var cera1 = document.getElementById("cera")
-var contenedor = document.getElementById("contenedor")
-var animacion = document.getElementById("animacion")
+var m_nombre_de_usurio = document.getElementById("m_nombre_de_usurio");
+var m_estado_de_cuenta = document.getElementById("m_estado_de_cuenta");
+var volver = document.getElementById("volver");
+boton.addEventListener("click", evaluar);
+cera.addEventListener("click", cerar);
+var cera1 = document.getElementById("cera");
+var contenedor = document.getElementById("contenedor");
+var animacion = document.getElementById("animacion");
 cera1.addEventListener("mouseover", cerar_seccion);
-cera1.addEventListener("mouseout", fuera_de_cerrar_seccion)
-cera1.addEventListener("click",cera)
-
+cera1.addEventListener("mouseout", fuera_de_cerrar_seccion);
 function fuera_de_cerrar_seccion() {
   cera1.style.opacity = "1";
   contenedor.style.opacity = "1";
@@ -28,7 +26,6 @@ function fuera_de_cerrar_seccion() {
 function cerar_seccion() {
   contenedor.style.opacity = "0.5";
   cera1.style.opacity = "1";
-
 }
 function cerar() {
   aler.style.display = "none";
@@ -63,13 +60,13 @@ console.log(localStorage.getItem("cuenta2"));
 var cuentas2 = JSON.parse(localStorage.getItem("cuenta2"));
 var cuenta3 = localStorage.setItem("cuenta3", JSON.stringify(P3));
 console.log(localStorage.getItem("cuenta3"));
-console.log(P1.apellido)
+console.log(P1.apellido);
 var cuentas3 = JSON.parse(localStorage.getItem("cuenta3"));
 //Funcion evaluar,toma las siguientes condiciones para evaluar  sobre el saldo y esto lo imprimimos en una tabla
 function evaluar() {
-  var negativo=-0;
+  var negativo = -0;
   var saldoActual = P1.saldo - retiro.value;
-  if (retiro.value >= P1.saldo || saldoActual <= 9 || retiro.value <negativo) {
+  if (retiro.value >= P1.saldo || saldoActual <= 9 || retiro.value < negativo) {
     if ((aler.style = "none" === "none")) {
       aler.style.display = "block";
     }
@@ -80,23 +77,23 @@ function evaluar() {
     SaldoActualizado.innerHTML = saldoActual;
     m_estado_de_cuenta.innerHTML = saldoActual;
     alert("El retiro se realizo de manera correcta");
-  } 
+  }
 }
 
 evaluar();
 
-function evaluarCredenciales(){
-  var input_nombre=localStorage.getItem("nombre_de_usuari")
- 
-  if ( input_nombre===P1.nombre) {
+function evaluarCredenciales() {
+  var input_nombre = localStorage.getItem("nombre_de_usuari");
+
+  if (input_nombre === P1.nombre) {
     m_nombre_de_usurio.innerHTML = P1.nombre;
-  m_estado_de_cuenta.innerHTML = P1.saldo;
-  }else if (input_nombre===P2.nombre){
+    m_estado_de_cuenta.innerHTML = P1.saldo;
+  } else if (input_nombre === P2.nombre) {
     m_nombre_de_usurio.innerHTML = P2.nombre;
     m_estado_de_cuenta.innerHTML = P2.saldo;
-  }else{
+  } else {
     m_nombre_de_usurio.innerHTML = P3.nombre;
     m_estado_de_cuenta.innerHTML = P3.saldo;
   }
 }
-evaluarCredenciales()
+evaluarCredenciales();
